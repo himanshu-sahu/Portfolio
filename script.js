85a7dfec9c6246938e70a52f9c7a89efaf3130f9
Himashu <himsahu.999@gmail.com>
2022-05-03 01:00:36 +0530

    All done except msg service

--- script.js
+++ script.js
@@ -97,4 +97,56 @@ typewriter.typeString('Full Stack Developer')
     .start();
 
     
+   
+    var responsiveSlider = function() {
+
+      var slider = document.getElementById("slider");
+      var sliderWidth = slider.offsetWidth;
+      var slideList = document.getElementById("slideWrap");
+      var count = 1;
+      var items = slideList.querySelectorAll("li").length;
+      var prev = document.getElementById("prev");
+      var next = document.getElementById("next");
+      
+      window.addEventListener('resize', function() {
+        sliderWidth = slider.offsetWidth;
+      });
+      
+      var prevSlide = function() {
+        if(count > 1) {
+          count = count - 2;
+          slideList.style.left = "-" + count * sliderWidth + "px";
+          count++;
+        }
+        else if(count = 1) {
+          count = items - 1;
+          slideList.style.left = "-" + count * sliderWidth + "px";
+          count++;
+        }
+      };
+      
+      var nextSlide = function() {
+        if(count < items) {
+          slideList.style.left = "-" + count * sliderWidth + "px";
+          count++;
+        }
+        else if(count = items) {
+          slideList.style.left = "0px";
+          count = 1;
+        }
+      };
+      
+      next.addEventListener("click",nextSlide);
+      
+      prev.addEventListener("click",prevSlide);
+      
+      setInterval(function() {
+        nextSlide()
+      }, 5000);
+      
+      };
+      
+    
+      responsiveSlider();  
+      
 navSlide();
